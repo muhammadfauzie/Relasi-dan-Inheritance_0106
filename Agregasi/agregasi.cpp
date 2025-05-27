d883274ddd4ac74a192fc353c1ed39f95fdb2ee1
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-27 18:33:52 +0700

    membuat member class ibu

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -11,3 +11,13 @@ int main() {
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("dewi");
+
+    varIbu->tambahanAnak(varAnak1);
+    varIbu->tambahanAnak(varAnak2);
+    varIbu->tambahAnak(varAnak3);
+    varIbu->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+ 
