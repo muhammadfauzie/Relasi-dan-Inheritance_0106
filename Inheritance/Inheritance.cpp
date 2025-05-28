49d6da3654856138747edd56174672fa3514e23b
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-28 20:58:03 +0700

    membuat class orang di dalam siswa

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -1,16 +1,48 @@
-#ifndef JANTUNG_H
-#define JANTUNG_H
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-class jantung {
+class orang {
     public:
-        jantung() {
-            cout << "Jantung dihidupkan\n";
+        string nama;
+
+        orang(string pNama):
+            nama(pNama) {
+            cout << "Orang \"" << nama << "\" ada\n";
         }
 
-        ~jantung() {
-            cout << "Jantung dimatikan\n";
+        ~orang() {
+            cout << "Orang dibuat\n" << endl;
+        }
+        ~orang() {
+            cout << "Orang dihapus\n" << endl; 
+        }
+        int jumlah(int a, int b) {
+            return a + b;
         }
 };
-#endif 
+
+class pelajar : public orang {
+    public:
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah): orang(pNama), sekolah(pSekolah) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+
+        ~pelajar() {
+            cout << "Pelajar dihapus\n" << endl;
+        }
+
+        string perkenalan() {
+            return "Halo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+        }
+};
+
+int main(){
+    pelajar siswa1("Andi laksono", "SMA 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
